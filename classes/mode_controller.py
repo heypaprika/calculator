84e442baf400ac5393ded406ebfd2380e0432cf7
CH Lee <heypaprika@naver.com>
2019-04-10 17:44:17 +0900

    modified mode_controlloer.py

--- classes/mode_controller.py
+++ classes/mode_controller.py
@@ -1,7 +1,9 @@
 class mode_controller:
-    def __init__(self, modenum):
-        # 뷰쪽 클래스 view
+    # rectangle 클래스 객체가 들어와서 해당 rectangle(frame)에 view해 줄 것.
+    def __init__(self, rectangle, modenum):
         self.modenum = modenum
+        # self.view = rectangle(modenum)
+        self.rectangle = rectangle
 
     @property
     def mode(self):
@@ -13,3 +15,4 @@ class mode_controller:
 
     def updateview(self):
         # self.modenum에 따라 view 보이기.
+        self.rectangle.view(modenum) # 이건 별로 같은데,,, 그럼 어떤 방식???
