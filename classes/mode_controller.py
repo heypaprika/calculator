3f8d79a3ae2dfbb37cc65c80c31caa8b3bca4939
CH Lee <heypaprika@naver.com>
2019-04-11 22:20:13 +0900

    modified mode_controller.py

--- classes/mode_controller.py
+++ classes/mode_controller.py
@@ -1,9 +1,11 @@
 class mode_controller:
     # rectangle 클래스 객체가 들어와서 해당 rectangle(frame)에 view해 줄 것.
-    def __init__(self, rectangle, modenum):
+    # tkinter에서는 각각 요소들을 row, col로 배치시키던데,, main, mode_controller 각각을 어디까지 해야하는지 고민,,
+    def __init__(self, frame, modenum):
         self.modenum = modenum
-        # self.view = rectangle(modenum)
-        self.rectangle = rectangle
+
+        # 뷰를 바꿀 프레임
+        self.frame = frame
 
     @property
     def mode(self):
@@ -14,5 +16,6 @@ class mode_controller:
         self.modenum = value
 
     def updateview(self):
-        # self.modenum에 따라 view 보이기.
-        self.rectangle.view(modenum) # 이건 별로 같은데,,, 그럼 어떤 방식???
+        # 해당하는 frame에 배치된 위젯 list만 가지고 와서
+        # 적절하게 frame을 보여주기
+        pass
