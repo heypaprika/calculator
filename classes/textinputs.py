6d7664823f13026ce9f81a401f1561fc9dd9e846
YunNaYoung <rosa3459@naver.com>
2019-04-11 19:34:29 +0900

    text_input

--- classes/textinputs.py
+++ classes/textinputs.py
@@ -1,2 +1,37 @@
+import tkinter as tk
+
 class textinputs:
-    pass
+
+    def __init__ (self):
+        window = Tk()
+        window.title("Calculation")
+        window.geometry("400x200")                          #ractangle size
+
+
+        frame1 = Frame(window)
+        frame1.pack()
+
+        self.name = StringVar(window,value='')
+        self.entryName = Entry(frame1, textvariable = self.name, width = 45)
+        self.entryName.grid(row = 1, column = 1)
+
+
+
+
+textinputs()
+
+'''
+window = tk.Tk()
+window.title("Calculation")
+window.geometry("400x200")                          #ractangle size
+
+
+frame1 = tk.Frame(window)
+frame1.pack()
+
+name = tk.StringVar(window,value='')
+entryName = tk.Entry(frame1, textvariable = name, width = 45)
+entryName.grid(row = 1, column = 1)
+
+window.mainloop()
+'''
