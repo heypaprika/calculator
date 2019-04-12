20ab400121c6ed5d306b53d73dd7b1c8bf7d05b9
CH Lee <heypaprika@naver.com>
2019-04-12 10:05:25 +0900

    modified mode_controller.py

--- classes/mode_controller.py
+++ classes/mode_controller.py
@@ -1,21 +1,29 @@
 class mode_controller:
     # rectangle 클래스 객체가 들어와서 해당 rectangle(frame)에 view해 줄 것.
     # tkinter에서는 각각 요소들을 row, col로 배치시키던데,, main, mode_controller 각각을 어디까지 해야하는지 고민,,
-    def __init__(self, frame, modenum):
-        self.modenum = modenum
+    def __init__(self, prevframe, afterframe):
 
-        # 뷰를 바꿀 프레임
-        self.frame = frame
+        self.prevframe = prevframe
+        self.afterframe = afterframe
+        self.updateview()
 
-    @property
-    def mode(self):
-        return self.modenum
+    # @property
+    # def mode(self):
+    #     return self.modenum
+    #
+    # @mode.setter
+    # def mode(self, value):
+    #     self.modenum = value
 
-    @mode.setter
-    def mode(self, value):
-        self.modenum = value
+    def on(self, frame):
+        print(frame)
+
+    def off(self, frame):
+        print(frame)
 
     def updateview(self):
         # 해당하는 frame에 배치된 위젯 list만 가지고 와서
         # 적절하게 frame을 보여주기
-        pass
+        self.off(self.prevframe)
+        self.on(self.afterframe)
+
--- main.py
+++ main.py
@@ -1,32 +1,5 @@
 import classes
 
 import tkinter as tk
-'''
-class textinputs:
 
-    def __init__ (self):
-        window = tk.Tk()
-        window.title("Calculation")
-        window.geometry("400x200")  # ractangle size
-
-        frame1 = tk.Frame(window)
-        frame1.pack()
-
-        name = tk.StringVar(window, value='')
-        entryName = tk.Entry(frame1, textvariable=name, width=45)
-        entryName.grid(row=1, column=1)
-
-
-
-textinputs()
-'''
-
-window = tk.Tk()
-window.title("Calculation")
-window.geometry("400x200")                          #ractangle size
-frame1 = tk.Frame(window)
-frame1.pack()
-name = tk.StringVar(window,value='3459')
-entryName = tk.Entry(frame1, textvariable = name, width = 45)
-entryName.grid(row = 1, column = 1)
-window.mainloop()
+classes.mode_controller(a,b)
