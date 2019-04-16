38185525e9920b0ded7993611a17bcba5cd0ba07
CH Lee <heypaprika@naver.com>
2019-04-16 16:10:24 +0900

    modified

--- classes/mode_controller.py
+++ classes/mode_controller.py
@@ -1,31 +1,33 @@
-class mode_controller:
-    # rectangle 클래스 객체가 들어와서 해당 rectangle(frame)에 view해 줄 것.
-    # tkinter에서는 각각 요소들을 row, col로 배치시키던데,, main, mode_controller 각각을 어디까지 해야하는지 고민,,
-    def __init__(self, prevframe, afterframe):
+class Mode:
+    def isVisible(self, bool):
+        return bool
 
-        self.prevframe = prevframe
-        self.afterframe = afterframe
+class rectangle(Mode):
+    def __init__(self, width=1, height=1):
+        self.width = width
+        self.height = height
+
+
+class mode_controller(rectangle):
+    def __init__(self, prev_rectangle, after_rectangle):
+        self.prev = prev_rectangle
+        self.after = after_rectangle
         self.updateview()
 
-    # @property
-    # def mode(self):
-    #     return self.modenum
-    #
-    # @mode.setter
-    # def mode(self, value):
-    #     self.modenum = value
-    #
-
-    def on(self, frame):
-        print(frame)
-
-    def off(self, frame):
-        print(frame)
-
     def updateview(self):
-        # 해당하는 frame에 배치된 위젯 list만 가지고 와서
-        # 적절하게 frame을 보여주기
-        self.off(self.prevframe)
-        self.on(self.afterframe)
+        a = self.prev.isVisible(True)
+        b = self.after.isVisible(False)
+        return a, b
 
-    # github test
+if __name__ == '__main__':
+    a_rect = rectangle(50,50)
+    # a_rect 를 button, textinputs 로 꾸미기.
+    #
+    #
+    #
+    b_rect = rectangle(60,60)
+    # b_rect 를 button, textinputs 로 꾸미기.
+    #
+    #
+    #
+    mode_controller(a_rect, b_rect)
